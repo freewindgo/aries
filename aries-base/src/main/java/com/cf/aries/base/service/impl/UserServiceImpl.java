79e51eee4aaf36981b9ef22459970d64ad2f2a61
于文硕 <yuwenshuo@passiontec.cn>
2018-05-29 14:45:42 +0800

    bug修改

--- aries-base/src/main/java/com/cf/aries/base/service/impl/UserServiceImpl.java
+++ aries-base/src/main/java/com/cf/aries/base/service/impl/UserServiceImpl.java
@@ -29,7 +29,7 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
-    public int insertUserInfo(UserInfo userInfo) {
+    public long insertUserInfo(UserInfo userInfo) {
         return userInfoMapper.insertSelective(userInfo);
     }
 
